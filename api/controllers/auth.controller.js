36f19237ee83a5cbd5ede1fd8f44dbf0cfa037b1
hardyal-singh <hardyalsinghdaudsar@gmail.com>
2024-06-02 12:00:52 +0530

    create middleware and error handlers functions for the possible error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,16 +1,16 @@
 import User from "../models/user.model.js";
 import bcrypt from 'bcrypt';
 
-const signup=(req, res)=>{
+const signup= async (req, res , next)=>{
     const {username, email, password}=req.body;
     const hashedPassword = bcrypt.hashSync(password, 10);
     const newUser= new User ({username, email, password:hashedPassword})
 
     try {
-        newUser.save();
+       await newUser.save();
         res.status(200).json({message:'user created successfully'})
     } catch (error) {
-        res.send(error.message);
+       next(error);
     }
 }
 
--- api/index.js
+++ api/index.js
@@ -16,6 +16,17 @@ app.use(express.json());
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 
+
+app.use((err, req,res, next)=>{
+    const statusCode =err.statusCode || 500;
+    const message =err.message || "Internal Server Error";
+
+    return res.status(statusCode).json({
+        statusCode,
+        message,
+    })
+})
+
 app.listen(3000, ()=>{
     console.log("listening on port 3000")
 })
