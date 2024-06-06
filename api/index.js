ef00652c9c457a6077e6a84a41a9b8b41304765d
hardyal-singh <hardyalsinghdaudsar@gmail.com>
2024-06-06 07:04:13 +0530

    create signin page UI and functionality

--- api/index.js
+++ api/index.js
@@ -22,6 +22,7 @@ app.use((err, req,res, next)=>{
     const message =err.message || "Internal Server Error";
 
     return res.status(statusCode).json({
+        status:false,
         statusCode,
         message,
     })
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Input from "../component/Input.jsx"
+import Button from '../component/Button.jsx';
+import {Link, useNavigate} from 'react-router-dom'
 
-export default function SignIn() {
+export default function SignOut() {
+  const [error, setError]=useState(null);
+  const [user, setUser]=useState(null);
+  const [loader, setLoader]=useState(null);
+
+  const navigate=useNavigate();
+
+const onchange =(e)=>{
+  setUser(pre=>({...pre, [e.target.name]:e.target.value}))
+}
+
+const onsubmit =async(e)=>{
+  e.preventDefault();
+try {
+  setLoader(true)
+  const res=await fetch ("/api/auth/signin", {
+    method:"POST",
+    headers:{
+      'Content-Type':"application/json"
+    },
+    body:JSON.stringify(user)
+  })
+  const data=await res.json();
+  console.log(data);
+  if(data.status==false){
+    setError(data.message);
+    setLoader(false);
+    return;
+  }
+  setLoader(false);
+  navigate('/')
+  
+
+} catch (error) {
+  setLoader(false);
+  setError(error.message);
+}
+
+}
+
   return (
-    <div>
-      SignIn page
+    <div className='mx-auto p-3 mx-w-lg sm:w-2/3'>
+      <h1 class="text-3xl text-center font-semibold my-7">Sign In</h1>
+      <form onSubmit={onsubmit} className='flex flex-col gap-4'>
+        <Input type="email" placeholder="Email" name="email" onChange={onchange}/>
+        <Input type="password" placeholder="Password" name="password" onChange={onchange}/>
+        <Button text={loader?'loading...':'Sign In'}/>
+      </form>
+      <div className='flex gap-2 mt-5'>
+        <p>Dont have an account?</p>
+        <Link to={'/sign-out'}>
+          <span className='text-blue-700'>Sign up</span>
+        </Link>
+      </div>
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
     </div>
   )
 }
