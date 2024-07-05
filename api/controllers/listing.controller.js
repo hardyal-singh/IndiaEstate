f6b1a8903067f4e8545085a136043df3c108b5f9
hardyal-singh <hardyalsinghdaudsar@gmail.com>
2024-07-05 07:37:16 +0530

    add upadate listing api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -8,20 +8,38 @@ const createListing = async (req, res, next) => {
   } catch (error) {
     next(error);
   }
-};    
+};
 
+const deleteListing = async (req, res, next) => {
+  const listing = await Listing.findById(req.params._id);
+  if (!listing) return next(errorHandler(404, "Listing not found"));
 
-const deleteListing = async (req, res, next) =>{
- const listing = await Listing.findById(req.params._id);
- if(!listing ) return next(errorHandler(404, "Listing not found"));
+  if (req.user.id !== listing.userRef)
+    return next(errorHandler(401, "You can delete your own listing."));
 
-if(req.user.id !==listing.userRef) return next(errorHandler(401,"You can delete your own listing."))
+  const result = await Listing.findByIdAndDelete(req.params._id);
+  console.log(result);
 
-const result= await Listing.findByIdAndDelete(req.params._id)
-console.log(result)
+  if (!result) return next(errorHandler(500, "failed to delete listing"));
+  return res.status(200).json({ message: "Listing deleted successfully." });
+};
 
-if(!result) return next (errorHandler(500, "failed to delete listing"))
-  return res.status(200).json({message:"Listing deleted successfully."})
-}
+const updateListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.findById(req.params._id);
+    if (!listing) return next(errorHandler(404, "Listing not found"));
+    if (listing.userRef !== req.user.id)
+      return next(errorHandler(404, "You can only update you own listing"));
 
-export { createListing, deleteListing};
+    const updatedListing = await Listing.findByIdAndUpdate(
+      req.params._id,
+      req.body,
+      { new: true }
+    );
+
+    return res.status(200).json(updatedListing);
+  } catch (error) {
+    next(error);
+  }
+};
+export { createListing, deleteListing, updateListing };
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,10 +1,12 @@
 import express from "express";
-import { createListing, deleteListing} from "../controllers/listing.controller.js";
+import { createListing, deleteListing, updateListing} from "../controllers/listing.controller.js";
 import { userVerify } from "../utils/userVerify.js";
 
 const listingRouter = express.Router();
 
 listingRouter.post("/create", userVerify, createListing);
 listingRouter.delete("/delete/:_id", userVerify, deleteListing);
+listingRouter.post("/update/:_id", userVerify, updateListing);
+
 
 export default listingRouter;
